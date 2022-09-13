8d56c4e06e74520a2f99d5d1e1644e18bdcf2fb3
Will Dumm <wrhdumm@gmail.com>
2022-09-13 11:40:08 -0700

    Add drift option and remove ifdef

--- tools/larch-usher.cpp
+++ tools/larch-usher.cpp
@@ -12,7 +12,6 @@
 #include "parsimony_score.hpp"
 #include "merge.hpp"
 #include <mpi.h>
-#define DIRECT_MAT
 MADAG optimize_dag_direct(const MADAG& dag);
 [[noreturn]] static void Usage() {
   std::cout << "Usage:\n";
@@ -124,32 +123,7 @@ int main(int argc, char** argv) {
     MADAG sample = weight.SampleTree({});
     check_edge_mutations(sample);
     MADAG result;
-    #ifdef DIRECT_MAT
     result=optimize_dag_direct(sample);
-    #else
-    sample.GetCompactGenomes() = sample.ComputeCompactGenomes(input_dag.GetReferenceSequence());
-    std::cout << " got sample tree and computed its compact genome.\n" << std::flush;
-
-    StoreTreeToProtobuf(sample, "sampled_tree.pb");
-    std::cout << "wrote sample tree " << std::to_string(i) << "\n" << std::flush;
-    CallMatOptimize(matoptimize_path, "sampled_tree.pb", "optimized_tree.pb");
-
-    std::cout << "ran matOptimize on the sample tree, and now we will try to recompute its compact genome\n" << std::flush;
-    sample.GetCompactGenomes() = sample.ComputeCompactGenomes(input_dag.GetReferenceSequence());
-    std::cout << "...computed its compact genome successfully.\n" << std::flush;
-
-    MADAG sample1 = LoadTreeFromProtobuf("sampled_tree.pb");
-    sample1.GetReferenceSequence() = input_dag.GetReferenceSequence();
-    sample1.GetCompactGenomes() = sample1.ComputeCompactGenomes(input_dag.GetReferenceSequence());
-    std::cout << " got sample tree and computed its compact genome.\n" << std::flush;
-
-    MADAG mo_dag = LoadTreeFromProtobuf("./optimized_tree.pb");
-    std::cout << "was able to read the matOptimize tree back from protobuf.\n" << std::flush;
-    mo_dag.GetReferenceSequence() = input_dag.GetReferenceSequence();
-    mo_dag.GetCompactGenomes() = mo_dag.ComputeCompactGenomes(mo_dag.GetReferenceSequence());
-    std::cout << "...computed its compact genome successfully.\n" << std::flush;
-    result=LoadTreeFromProtobuf("optimized_tree.pb");
-    #endif
     optimized_dags.push_back(std::move(result));
     merge.AddDAGs({optimized_dags.back()});
   }
