56ba307342b0656722fc9a4248d7b430929c7376
Ognian Milanov <ognian.milanov@gmail.com>
2022-09-21 20:12:23 +0300

    Added empty traversal iterators test

---
+++ test/test_traversal_iterators.cpp
@@ -0,0 +1,10 @@
+#include "test_common.hpp"
+#include "dag.hpp"
+
+[[maybe_unused]] static void test_traversal_iterators() {
+  
+}
+
+[[maybe_unused]] static const auto test_added0 =
+    add_test({[] { test_traversal_iterators(); },
+              "Traversal iterators"});
