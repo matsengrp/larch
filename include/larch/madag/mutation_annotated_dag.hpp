5cf3902b48d54a5abe91c0193e4870895ca51f7e
david.rich27 <david.rich@umotana.edu>
2023-04-24 10:28:00 -0600

    Added MutationBase class.  WIP: Replacing uses of char with MutationBase in MADAG.

--- include/larch/madag/mutation_annotated_dag.hpp
+++ include/larch/madag/mutation_annotated_dag.hpp
@@ -27,13 +27,91 @@
 #include "larch/madag/compact_genome.hpp"
 #include "larch/madag/edge_mutations.hpp"
 
+// Strongly typed class to represent a base
+struct MutationBase {
+  using BitArray = std::array<bool, 2>;
+
+  MutationBase(const BitArray m_value) { value = m_value; };
+  MutationBase(const char m_char_in) {
+    for (const auto &[m_base, m_char] : mut_to_char_map) {
+      if (m_char_in == m_char) {
+        value = m_base.value;
+        return;
+      }
+    }
+    // ASSERT(false, "ERROR: Invalid char given for MutationBase constructor.");
+  }
+
+  // NOTE: Due to mapping, we can simply invert the bool vector to get a base's
+  // complement.
+  MutationBase GetComplement() const {
+    MutationBase m_out{{!value[0], !value[1]}};
+    return m_out;
+  }
+
+  char ToChar() const { return mut_to_char_map.find(value)->second; }
+
+  friend std::ostream &operator<<(std::ostream &os, const MutationBase m_in) {
+    os << m_in.ToChar();
+    return os;
+  }
+
+  static std::string ToString(std::vector<MutationBase> m_in) {
+    std::string str_out = "";
+    for (size_t i = 0; i < m_in.size(); i++) {
+      str_out[i] += m_in[i].ToChar();
+    }
+    return str_out;
+  }
+
+  friend std::ostream &operator<<(std::ostream &os,
+                                  const std::vector<MutationBase> m_in) {
+    os << ToString(m_in);
+    return os;
+  }
+
+  friend bool operator==(const MutationBase &lhs, const MutationBase &rhs) {
+    return lhs.value == rhs.value;
+  }
+  friend bool operator<(const MutationBase &lhs, const MutationBase &rhs) {
+    return lhs.value < rhs.value;
+  }
+  friend bool operator==(const MutationBase &lhs, const BitArray &rhs) {
+    return lhs.value == rhs;
+  }
+  friend bool operator==(const MutationBase &lhs, const char &rhs) {
+    return lhs.ToChar() == rhs;
+  }
+
+  // Two-bit array represents the four possible DNA bases.
+  BitArray value;
+
+  struct DNA {
+    static const MutationBase A, C, G, T;
+  };
+  static const std::array<MutationBase, 4> bases;
+  static const std::map<MutationBase, char> mut_to_char_map;
+};
+const MutationBase MutationBase::DNA::A = MutationBase({0, 0});
+const MutationBase MutationBase::DNA::C = MutationBase({0, 1});
+const MutationBase MutationBase::DNA::G = MutationBase({1, 0});
+const MutationBase MutationBase::DNA::T = MutationBase({1, 1});
+const std::array<MutationBase, 4> MutationBase::bases = {
+    MutationBase::DNA::A, MutationBase::DNA::C, MutationBase::DNA::G,
+    MutationBase::DNA::T};
+const std::map<MutationBase, char> MutationBase::mut_to_char_map = {
+    {MutationBase::DNA::A, 'A'},
+    {MutationBase::DNA::C, 'C'},
+    {MutationBase::DNA::G, 'G'},
+    {MutationBase::DNA::T, 'T'}};
+
 struct ReferenceSequence {
   std::string reference_sequence_;
 };
 
 template <typename CRTP, typename Tag>
 struct FeatureConstView<ReferenceSequence, CRTP, Tag> {
-  const std::string& GetReferenceSequence() const;
+  const std::string &GetReferenceSequence() const;
   void AssertUA() const;
   bool HaveUA() const;
 };
@@ -41,7 +119,7 @@ struct FeatureConstView<ReferenceSequence, CRTP, Tag> {
 template <typename CRTP, typename Tag>
 struct FeatureMutableView<ReferenceSequence, CRTP, Tag> {
   void SetReferenceSequence(std::string_view reference_sequence) const;
-  void AddUA(const EdgeMutations& mutations_at_root) const;
+  void AddUA(const EdgeMutations &mutations_at_root) const;
   void RecomputeCompactGenomes() const;
   void RecomputeEdgeMutations() const;
 };
@@ -52,12 +130,12 @@ struct SampleId {
 
 template <typename CRTP, typename Tag>
 struct FeatureConstView<SampleId, CRTP, Tag> {
-  const std::optional<std::string>& GetSampleId() const;
+  const std::optional<std::string> &GetSampleId() const;
 };
 
 template <typename CRTP, typename Tag>
 struct FeatureMutableView<SampleId, CRTP, Tag> {
-  void SetSampleId(const std::optional<std::string>& sample_id) const;
+  void SetSampleId(const std::optional<std::string> &sample_id) const;
 };
 
 #include "larch/impl/madag/mutation_annotated_dag_impl.hpp"
