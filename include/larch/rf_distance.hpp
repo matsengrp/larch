04c18ce66c6226ceaefc1529efc9226ccce3a3b3
Ognian Milanov <ognian.milanov@gmail.com>
2023-09-12 15:42:25 +0300

    WIP

--- include/larch/rf_distance.hpp
+++ include/larch/rf_distance.hpp
@@ -1,8 +1,22 @@
+/*
+computing the summed RF distance over all trees in the DAG.
+This routine requires 4 traversals of the nodes in DAG to calculate the rf distance
+sums:
+- one postorder traversal to assign subtree counts below each node, and
+- one in a preorder traversal that uses the subtree counts and computes above-tree
+counts
+- a loop over the nodes (in any order) accumulates the two types of counts and saves
+them in an accumulation based on unique leafsets
+- finally, a postorder traversal to calculate the sums of RF distances between trees in
+a dag and all the trees in a reference, using the methods provided by a
+SubtreeWeight<SumRFDistance, DAGTYPE> object.
+*/
 #pragma once
 
 #include "larch/subtree/subtree_weight.hpp"
+#include "larch/subtree/tree_count.hpp"
 #include "larch/subtree/simple_weight_ops.hpp"
-#include "larch/merge/leaf_set.hpp"
+#include "larch/merge/merge.hpp"
 
 template <typename SubtreeWeight, typename Node>
 ArbitraryInt ComputeAboveTreeCount(Node node,
@@ -18,7 +32,7 @@ ArbitraryInt ComputeAboveTreeCount(Node node,
 
   if (above_tree_counts.at(node.GetId().value) > 0) {
     return above_tree_counts.at(node.GetId().value);
-  } else if (node.IsUANode()) {
+  } else if (node.IsUA()) {
     above_tree_counts.at(node.GetId().value) = 1;
     return 1;
   }
@@ -30,7 +44,7 @@ ArbitraryInt ComputeAboveTreeCount(Node node,
     ArbitraryInt below_parent = 1;
 
     for (auto clade : parent_node.GetClades()) {
-      if (clade != current_clade) {
+      if (not(clade == current_clade)) {
         // sum up the list of values {cached_below_tree_counts[edge] : edge in clade}
         auto below_clade = ranges::accumulate(
             parent_node.GetClade(clade) |
@@ -48,25 +62,25 @@ ArbitraryInt ComputeAboveTreeCount(Node node,
                                    cached_below_tree_counts) *
              below_parent;
   }
-  above_tree_counts.at(node.getId().value) = above;
+  above_tree_counts.at(node.GetId().value) = above;
   return above;
 }
 
 // Create a BinaryOperatorWeightOps for computing sum RF distances to the provided
 // reference DAG (using SimpleWeightOps):
-template <typename REFDAG>
 struct SumRFDistance_ {
   using Weight = ArbitraryInt;
+  static inline Weight Identity = 0;
   ArbitraryInt num_trees_in_dag;
-  std::unordered_map<LeafSet, ArbitraryInt> leafset_to_full_treecount;
+  std::unordered_map<const LeafSet*, ArbitraryInt> leafset_to_full_treecount;
   ArbitraryInt shift_sum;
 
-  explicit SumRFDistance_(REFDAG reference_dag) {
-    SubtreeWeight<TreeCount, MADAG> below_tree_counts{reference_dag};
+  explicit SumRFDistance_(Merge& reference_dag) {
+    SubtreeWeight<TreeCount, MergeDAG> below_tree_counts{reference_dag.GetResult()};
     std::vector<ArbitraryInt> above_tree_counts;
-    above_tree_counts.resize(reference_dag.GetNodesCount());
-    num_trees_in_dag = below_tree_counts(reference_dag.GetRoot());
-    auto above_root_node = ComputeAboveTreeCount(reference_dag.GetRoot(),
+    above_tree_counts.resize(reference_dag.GetResult().GetNodesCount());
+    num_trees_in_dag = below_tree_counts(reference_dag.GetResult().GetRoot());
+    auto above_root_node = ComputeAboveTreeCount(reference_dag.GetResult().GetRoot(),
                                                  above_tree_counts, below_tree_counts);
 
     // create a list of unique (topologically) nodes in the DAG, and accumulate
@@ -75,7 +89,7 @@ struct SumRFDistance_ {
     for (auto node : reference_dag.GetResult().GetNodes()) {
       if (not node.IsUA()) {
         leafset_to_full_treecount
-            [reference_dag.GetResultNodeLabels().at(node.GetId().value).GetLeafSet()] +=
+            [reference_dag.GetResultNodeLabels().at(node.GetId()).GetLeafSet()] +=
             above_tree_counts[node.GetId().value] * below_tree_counts[node];
       }
     }
@@ -90,16 +104,16 @@ struct SumRFDistance_ {
   }
 
   template <typename DAG>
-  static Weight ComputeLeaf(DAG dag, NodeId node_id) {
+  static Weight ComputeLeaf(DAG, NodeId) {
     return 0;
   }
 
   template <typename DAG>
   Weight ComputeEdge(DAG dag, EdgeId edge_id) {
-    auto edge = dag.GetEdge(edge_id);
+    auto edge = dag.Get(edge_id);
     // clade should be a LeafSet, the key type in the mapping
     // leafset_to_full_treecount:
-    auto clade = edge.parent.GetClade(edge.clade);
+    auto clade = edge.GetParent().GetClade(edge.GetClade());
     auto record = leafset_to_full_treecount.find(clade);
     if (record == leafset_to_full_treecount.end()) {
       return num_trees_in_dag;
@@ -128,39 +142,34 @@ struct SumRFDistance_ {
 };
 
 // Not sure if you can substruct a templated struct like this...?
-template <typename REFDAG>
-struct SumRFDistance : SimpleWeightOps<SumRFDistance_<REFDAG>> {
-  explicit SumRFDistance(REFDAG reference_dag)
-      : SimpleWeightOps<SumRFDistance_<REFDAG>>{SumRFDistance_{reference_dag}} {}
+struct SumRFDistance : SimpleWeightOps<SumRFDistance_> {
+  explicit SumRFDistance(Merge& reference_dag)
+      : SimpleWeightOps<SumRFDistance_>{SumRFDistance_{reference_dag}} {}
 };
 
 // Create a WeightOps for computing RF distances to the provided reference tree:
-template <typename REFDAG>
-struct RFDistance : SumRFDistance<REFDAG> {
-  explicit RFDistance(REFDAG reference_dag) {
-    Assert(reference_dag.IsTree());
+struct RFDistance : SumRFDistance {
+  explicit RFDistance(Merge& reference_dag) : SumRFDistance{reference_dag} {
+    Assert(reference_dag.GetResult().IsTree());
     // now behave exactly like SumRFDistance
   }
 };
 
-template <typename REFDAG>
-struct MaxSumRFDistance_ : SumRFDistance_<REFDAG> {
-  using Weight = typename SumRFDistance_<REFDAG>::Weight;
+struct MaxSumRFDistance_ : SumRFDistance_ {
+  using Weight = typename SumRFDistance_::Weight;
+  explicit MaxSumRFDistance_(Merge& reference_dag) : SumRFDistance_{reference_dag} {}
   bool Compare(Weight lhs, Weight rhs) { return lhs > rhs; }
 };
 
-template <typename REFDAG>
-struct MaxSumRFDistance : SimpleWeightOps<MaxSumRFDistance_<REFDAG>> {
-  explicit MaxSumRFDistance(REFDAG reference_dag)
-      : SimpleWeightOps<MaxSumRFDistance_<REFDAG>>{
-            MaxSumRFDistance_<REFDAG>{reference_dag}} {}
+struct MaxSumRFDistance : SimpleWeightOps<MaxSumRFDistance_> {
+  explicit MaxSumRFDistance(Merge& reference_dag)
+      : SimpleWeightOps<MaxSumRFDistance_>{MaxSumRFDistance_{reference_dag}} {}
 };
 
 // Create a WeightOps for computing RF distances to the provided reference tree:
-template <typename REFDAG>
-struct MaxRFDistance : MaxSumRFDistance<REFDAG> {
-  explicit MaxRFDistance(REFDAG reference_dag) {
-    Assert(reference_dag.IsTree());
+struct MaxRFDistance : MaxSumRFDistance {
+  explicit MaxRFDistance(Merge& reference_dag) : MaxSumRFDistance{reference_dag} {
+    Assert(reference_dag.GetResult().IsTree());
     // now behave exactly like MaxSumRFDistance
   }
 };
--- test/test_rf_distance.cpp
+++ test/test_rf_distance.cpp
@@ -1,8 +1,19 @@
 #include "larch/rf_distance.hpp"
 
 #include "test_common.hpp"
+#include "sample_dag.hpp"
+#include "larch/subtree/subtree_weight.hpp"
 
-static void test_rf_distance() {}
+static void test_rf_distance() {
+  auto storage = MakeSampleDAG();
+  auto view = storage.View();
+  Merge merge{view.GetReferenceSequence()};
+  merge.AddDAG(view);
+  auto dag = merge.GetResult();
+  SubtreeWeight<SumRFDistance, std::decay_t<decltype(dag)>> count{dag};
+  auto score = count.ComputeWeightBelow(dag.GetRoot(), RFDistance{merge});
+  std::cout << "RF distance: " << score << "\n";
+}
 
 [[maybe_unused]] static const auto test_added0 =
     add_test({[] { test_rf_distance(); }, "RF distance"});
