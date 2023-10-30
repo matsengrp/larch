54ebb862935f9959ab06bcfe3338eae859ce7a55
marybarker <marybarker103@gmail.com>
2023-10-30 14:16:50 -0700

    forgot one updated test change

--- test/test_weight_accum.cpp
+++ test/test_weight_accum.cpp
@@ -29,7 +29,7 @@ static void test_weight_accum(std::string_view path, Weight expected_score) {
   MADAGStorage dag = LoadDAGFromProtobuf(path);
   auto dag_view = dag.View();
   dag_view.RecomputeCompactGenomes(true);
-  dag_view.SampleIdsFromCG();
+  dag_view.SampleIdsFromCG(true);
   test_weight_accum(dag_view, expected_score);
 }
 
