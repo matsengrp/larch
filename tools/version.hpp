0136797764fe966f54eee6a5068d3de651f4df8c
David Rich <david.rich27@gmail.com>
2024-08-26 22:09:50 -0600

    wip: cleanup github actions.

--- tools/version.hpp
+++ tools/version.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
-#define VERSION_NUMBER "v0.1.0-dev"
-#define GIT_COMMIT_HASH "a401244"
-#define GIT_COMMIT_DATE "2024-08-21"
+#define VERSION_NUMBER "v0.1.0-dev-dev"
+#define GIT_COMMIT_HASH "9791f5a"
+#define GIT_COMMIT_DATE "2024-08-26"
--- version.cmake
+++ version.cmake
@@ -1,10 +1,31 @@
 # set the version from VERSION file, and the build commit and date from git repo
 
-message(STATUS "cmake source directory ${CMAKE_SOURCE_DIR}")
+# get git version tag
+execute_process(
+    COMMAND git describe --tags --abbrev=0
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE GIT_TAG_VERSION
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_QUIET
+)
 
-file(READ "${CMAKE_SOURCE_DIR}/VERSION" VERSION_NUMBER)
-string(STRIP "${VERSION_NUMBER}" VERSION_NUMBER)
+# get version from git tag if available, else fall back to VERSION file
+if(GIT_TAG_VERSION)
+    set(VERSION_NUMBER ${GIT_TAG_VERSION})
+    message("using version from git tag: ${VERSION_NUMBER}")
+else()
+    file(READ "${CMAKE_SOURCE_DIR}/VERSION" VERSION_NUMBER)
+    string(STRIP "${VERSION_NUMBER}" VERSION_NUMBER)
+    set(VERSION_NUMBER "${VERSION_NUMBER}-dev")
+    message("using version from VERSION file: ${VERSION_NUMBER}")
+endif()
 
+# get version from VERSION file
+# file(READ "${CMAKE_SOURCE_DIR}/VERSION" VERSION_NUMBER)
+# string(STRIP "${VERSION_NUMBER}" VERSION_NUMBER)
+# set(VERSION_NUMBER "${VERSION_NUMBER}")
+
+# get git hash
 execute_process(
     COMMAND git rev-parse --short HEAD
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
@@ -12,6 +33,7 @@ execute_process(
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
+# get git date
 execute_process(
     COMMAND git log -1 --format=%cd --date=short
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
@@ -19,6 +41,7 @@ execute_process(
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
+# configure version file
 configure_file(
     "tools/version.hpp.in"
     "tools/version.hpp"
