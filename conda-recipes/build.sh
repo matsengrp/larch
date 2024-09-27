00dd46ffa34969079c74773b3c0de858e0c207ef
David Rich <david.rich27@gmail.com>
2024-09-27 10:28:47 -0700

    update to match conda-forge changes

--- conda-recipes/build.sh
+++ conda-recipes/build.sh
@@ -9,12 +9,14 @@ CMAKE_USE_USHER=${USE_USHER:-"ON"}
 CMAKE_NUM_THREADS=${CMAKE_NUM_THREADS:-"4"}
 MAKE_NUM_THREADS=${MAKE_NUM_THREADS:-"20"}
 INCLUDE_LARCH_TEST=${INCLUDE_LARCH_TEST:-"false"}
+RUN_LARCH_TEST=${INCLUDE_LARCH_TEST:-"true"}
 
-echo "INCLUDE_LARCH_TEST: ${INCLUDE_LARCH_TEST}"
 echo "CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}"
 echo "CMAKE_USE_USHER: ${CMAKE_USE_USHER}"
 echo "CMAKE_NUM_THREADS: ${CMAKE_NUM_THREADS}"
 echo "MAKE_NUM_THREADS: ${MAKE_NUM_THREADS}"
+echo "INCLUDE_LARCH_TEST: ${INCLUDE_LARCH_TEST}"
+echo "RUN_LARCH_TEST: ${RUN_LARCH_TEST}"
 
 export CMAKE_NUM_THREADS=${CMAKE_NUM_THREADS}
 cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DUSE_USHER=${CMAKE_USE_USHER} ..
@@ -22,6 +24,11 @@ make -j${MAKE_NUM_THREADS}
 
 mkdir -p $PREFIX/lib
 cp $(find . -name *.so*) $PREFIX/lib/
+
+if [[ ${RUN_LARCH_TEST} == true ]]; then
+    ln -s ../data
+    ./larch-test -tag slow
+fi
 
 mkdir -p $PREFIX/bin
 cp larch-usher $PREFIX/bin/larch-usher
@@ -31,4 +38,3 @@ cp larch-dag2dot $PREFIX/bin/larch-dag2dot
 if [[ ${INCLUDE_LARCH_TEST} == true ]]; then
     cp larch-test $PREFIX/bin/larch-test
 fi
-
