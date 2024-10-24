a058b7101459dcd2d9f16eb613a0229fd96f86bf
David Rich <david.rich27@gmail.com>
2024-10-24 10:16:47 -0700

    wip: updates to conda environment and to shared libraries in cmake.

--- conda-recipes/build.sh
+++ conda-recipes/build.sh
@@ -19,22 +19,15 @@ echo "LARCH_INCLUDE_TEST: ${LARCH_INCLUDE_TEST}"
 echo "LARCH_RUN_TEST: ${LARCH_RUN_TEST}"
 
 export CMAKE_NUM_THREADS=${CMAKE_NUM_THREADS}
-cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DUSE_USHER=${CMAKE_USE_USHER} ..
+cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DUSE_USHER=${CMAKE_USE_USHER} -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 make -j${MAKE_NUM_THREADS}
-
-mkdir -p $PREFIX/lib
-cp $(find . -name *.so*) $PREFIX/lib/
 
 if [[ ${LARCH_RUN_TEST} == true ]]; then
     ln -s ../data
     ./larch-test -tag slow
 fi
 
-mkdir -p $PREFIX/bin
-cp larch-usher $PREFIX/bin/larch-usher
-cp larch-dagutil $PREFIX/bin/larch-dagutil
-cp larch-dag2dot $PREFIX/bin/larch-dag2dot
+make install
 
-if [[ ${LARCH_INCLUDE_TEST} == true ]]; then
-    cp larch-test $PREFIX/bin/larch-test
-fi
+mkdir -p ${PREFIX}/lib
+cp $(find . -name *.so*) ${PREFIX}/lib
