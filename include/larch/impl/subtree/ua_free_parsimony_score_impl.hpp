c2ab41653fc366aed73e13bd8d7a8d7680ddde7e
Ognian Milanov <ognian.milanov@gmail.com>
2025-12-04 17:58:43 +0200

    Optionally ignoring mutations on the UA edges (#102)
    * add parsimony scorer that ignores edge mutations on children of the UA node
    
    * make return implementations uniform
    
    * Added the test
    
    * fix test
    
    * added explanatory option to README
    
    ---------
    
    Co-authored-by: marybarker <marybarker103@gmail.com>

---
+++ include/larch/impl/subtree/ua_free_parsimony_score_impl.hpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+
+template <typename DAG>
+UAFreeParsimonyScore::Weight UAFreeParsimonyScore::ComputeLeaf(DAG, NodeId) {
+  return 0;
+}
+
+template <typename DAG>
+UAFreeParsimonyScore::Weight UAFreeParsimonyScore::ComputeEdge(DAG dag, EdgeId edge_id) {
+  if (dag.Get(edge_id).GetParent().IsUA()) {
+    return 0;
+  }
+  return dag.Get(edge_id).GetEdgeMutations().size();
+}
+
+std::pair<UAFreeParsimonyScore::Weight, std::vector<size_t>>
+UAFreeParsimonyScore::WithinCladeAccumOptimum(
+    const std::vector<UAFreeParsimonyScore::Weight>& inweights) {
+  UAFreeParsimonyScore::Weight optimal_weight = std::numeric_limits<size_t>::max();
+  std::vector<size_t> optimal_indices;
+  size_t inweight_idx = 0;
+  for (auto weight : inweights) {
+    if (weight < optimal_weight) {
+      optimal_weight = weight;
+      optimal_indices.clear();
+      optimal_indices.push_back(inweight_idx);
+    } else if (weight == optimal_weight) {
+      optimal_indices.push_back(inweight_idx);
+    }
+    inweight_idx++;
+  }
+  return {optimal_weight, optimal_indices};
+}
+
+UAFreeParsimonyScore::Weight UAFreeParsimonyScore::BetweenClades(
+    const std::vector<UAFreeParsimonyScore::Weight>& inweights) {
+  return std::accumulate(inweights.begin(), inweights.end(),
+                         static_cast<UAFreeParsimonyScore::Weight>(0));
+}
+
+UAFreeParsimonyScore::Weight UAFreeParsimonyScore::AboveNode(
+    UAFreeParsimonyScore::Weight edgeweight, UAFreeParsimonyScore::Weight childnodeweight) {
+  return edgeweight + childnodeweight;
+}
+
