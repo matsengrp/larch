6ba1fbac1d4efc702fbe6547cc3a486642c7d942
Ognian Milanov <ognian.milanov@gmail.com>
2026-02-20 05:44:59 +0000

    Removed mat_mtx_ lock, guard check_edge_mutations

--- include/larch/impl/spr/batching_callback_impl.hpp
+++ include/larch/impl/spr/batching_callback_impl.hpp
@@ -38,66 +38,62 @@ bool BatchingCallback<CRTP>::operator()(Profitable_Moves& move, int best_score_c
     }
   });
 
-  {
-    std::shared_lock lock{mat_mtx_};
-
-    Assert(sample_mat_storage_ != nullptr);
-    auto spr =
-        std::make_unique<SPRType>(AddSPRStorage(sample_mat_storage_->View()));
+  Assert(sample_mat_storage_ != nullptr);
+  auto spr =
+      std::make_unique<SPRType>(AddSPRStorage(sample_mat_storage_->View()));
 
 #ifdef KEEP_ASSERTS
-    for (auto i : spr->View().Const().GetLeafs()) {
-      Assert(i.HaveSampleId());
-    }
-    auto old_src_parent = spr->View().GetNodeFromMAT(move.src->parent);
-#endif
-
-    if (!spr->View().InitHypotheticalTree(move, nodes_with_major_allele_set_change)) {
-      return false;
-    }
-
-    auto fragment = std::make_unique<FragmentType>(
-        collapse_empty_fragment_edges_ ? spr->View().MakeFragment()
-                                       : spr->View().MakeUncollapsedFragment());
-
-#ifdef KEEP_ASSERTS
-    auto new_old_src_parent = spr->View().GetOldSourceParent();
-    Assert(old_src_parent.GetId() == new_old_src_parent.GetId());
-#endif
-
-    auto frag_view = fragment->View();
-    auto& impl = static_cast<CRTP&>(*this);
-    std::pair<bool, bool> accepted = impl.OnMove(
-        spr->View(), frag_view, move, best_score_change,
-        nodes_with_major_allele_set_change);
-
-    if (accepted.first) {
-#ifdef KEEP_ASSERTS
-      for (auto node : frag_view.GetNodes()) {
-        if (not(node.IsUA() or node.IsMoveNew())) {
-          Assert(node.GetId().value != NoId);
-          if (node.GetOld().IsLeaf()) {
-            Assert(node.GetOld().HaveSampleId());
-            Assert(not node.GetOld().GetSampleId().value().empty());
-          }
-        }
-      }
-      for (auto edge : frag_view.GetEdges()) {
-        Assert(edge.GetId().value != NoId);
-        Assert(edge.GetChild().GetId().value != NoId);
-        if (not edge.GetParent().IsUA()) {
-          Assert(edge.GetParent().GetId().value != NoId);
-        }
-      }
-#endif
-      moves_batch_.AddElement([&](auto& bucket) {
-        bucket.push_back(MoveStorage{std::move(spr), std::move(fragment)});
-      });
-      applied_moves_count_.fetch_add(1);
-    }
-
-    return accepted.second;
+  for (auto i : spr->View().Const().GetLeafs()) {
+    Assert(i.HaveSampleId());
   }
+  auto old_src_parent = spr->View().GetNodeFromMAT(move.src->parent);
+#endif
+
+  if (!spr->View().InitHypotheticalTree(move, nodes_with_major_allele_set_change)) {
+    return false;
+  }
+
+  auto fragment = std::make_unique<FragmentType>(
+      collapse_empty_fragment_edges_ ? spr->View().MakeFragment()
+                                     : spr->View().MakeUncollapsedFragment());
+
+#ifdef KEEP_ASSERTS
+  auto new_old_src_parent = spr->View().GetOldSourceParent();
+  Assert(old_src_parent.GetId() == new_old_src_parent.GetId());
+#endif
+
+  auto frag_view = fragment->View();
+  auto& impl = static_cast<CRTP&>(*this);
+  std::pair<bool, bool> accepted = impl.OnMove(
+      spr->View(), frag_view, move, best_score_change,
+      nodes_with_major_allele_set_change);
+
+  if (accepted.first) {
+#ifdef KEEP_ASSERTS
+    for (auto node : frag_view.GetNodes()) {
+      if (not(node.IsUA() or node.IsMoveNew())) {
+        Assert(node.GetId().value != NoId);
+        if (node.GetOld().IsLeaf()) {
+          Assert(node.GetOld().HaveSampleId());
+          Assert(not node.GetOld().GetSampleId().value().empty());
+        }
+      }
+    }
+    for (auto edge : frag_view.GetEdges()) {
+      Assert(edge.GetId().value != NoId);
+      Assert(edge.GetChild().GetId().value != NoId);
+      if (not edge.GetParent().IsUA()) {
+        Assert(edge.GetParent().GetId().value != NoId);
+      }
+    }
+#endif
+    moves_batch_.AddElement([&](auto& bucket) {
+      bucket.push_back(MoveStorage{std::move(spr), std::move(fragment)});
+    });
+    applied_moves_count_.fetch_add(1);
+  }
+
+  return accepted.second;
 }
 
 template <typename CRTP>
@@ -112,7 +108,9 @@ void BatchingCallback<CRTP>::operator()(MAT::Tree& tree) {
   auto reassigned_states = reassigned_states_storage_->View();
   reassigned_states.BuildFromMAT(tree, merge_.GetResult().GetReferenceSequence());
 
+#ifdef KEEP_ASSERTS
   check_edge_mutations(reassigned_states.Const());
+#endif
   reassigned_states.RecomputeCompactGenomes(true);
   {
     auto all = moves_batch_.GatherAndClear([this](auto buckets) {
@@ -171,7 +169,9 @@ void BatchingCallback<CRTP>::OnReassignedStates(MAT::Tree& tree) {
   auto reassigned_states = reassigned_states_storage_->View();
   reassigned_states.BuildFromMAT(tree, merge_.GetResult().GetReferenceSequence());
   reassigned_states.RecomputeCompactGenomes();
+#ifdef KEEP_ASSERTS
   check_edge_mutations(reassigned_states.Const());
+#endif
   reassigned_states.RecomputeCompactGenomes(false);
   {
     std::unique_lock lock{merge_mtx_};
@@ -260,7 +260,9 @@ void BatchingCallback<CRTP>::CreateMATStorage(MAT::Tree& tree,
       std::make_unique<MATStorage>(AddMATConversion(Storage::EmptyDefault()));
   auto view = sample_mat_storage_->View();
   view.BuildFromMAT(tree, ref_seq);
+#ifdef KEEP_ASSERTS
   check_edge_mutations(view.Const());
+#endif
   view.RecomputeCompactGenomes(true);
   view.SampleIdsFromCG();
 }
